4a36af21b5d00821168d3845c0c17d654d44dd7a
Garanah <151655622+Garanah@users.noreply.github.com>
2024-04-13 17:01:57 -0400

    Animation Arms
    ADD

--- Assets/Script/FoodLauncher.cs
+++ Assets/Script/FoodLauncher.cs
@@ -10,32 +10,82 @@ public class FoodLauncher : MonoBehaviour
     public float foodSpeed = 10;
     public int foodNumberUnlocked = 2;
 
+<<<<<<< Updated upstream
     private int chosenFoodIndex = 0;
+=======
+    public Animator animatorhand;
+    private bool isClick = false;
+    private bool isTurnPage = false;
+
+    private int foodIndex = 0;
+>>>>>>> Stashed changes
+
+    void start()
+    {
+        animatorhand = GetComponent<Animator>();
+    }
 
     void Update()
     {
+        animatorhand.SetBool("isClick", isClick);
+        animatorhand.SetBool("isTurnPage", isTurnPage);
+
         //if mouse down
         if (Input.GetMouseButtonDown(0))
         {
+<<<<<<< Updated upstream
             var bullet = Instantiate(foodList[chosenFoodIndex], foodSpawnPoint.position + foodSpawnPoint.forward*2, foodSpawnPoint.rotation);
+=======
+            isClick = true;
+
+            var bullet = Instantiate(foodList[foodIndex], foodSpawnPoint.position + foodSpawnPoint.forward*2, foodSpawnPoint.rotation);
+>>>>>>> Stashed changes
             bullet.GetComponent<Rigidbody>().velocity = foodSpawnPoint.forward * foodSpeed;
         }
+        else
+        {
+            isClick = false;
+        }
+
 
         //if mouse wheel is scrolled
         if (Input.mouseScrollDelta.y > 0)
         {
+<<<<<<< Updated upstream
             chosenFoodIndex++;
             if (chosenFoodIndex >= foodList.Count || chosenFoodIndex >= foodNumberUnlocked)
+=======
+            isTurnPage = true;
+            foodIndex++;
+            if (foodIndex >= foodList.Count)
+>>>>>>> Stashed changes
             {
                 chosenFoodIndex = 0;
             }
+            else
+            {
+                isTurnPage = false;
+            }
         }
+
+
         else if (Input.mouseScrollDelta.y < 0)
         {
+<<<<<<< Updated upstream
             chosenFoodIndex--;
             if (chosenFoodIndex < 0)
+=======
+            
+            isTurnPage = true;
+            foodIndex--;
+            if (foodIndex < 0)
+>>>>>>> Stashed changes
             {
                 chosenFoodIndex = foodList.Count - 1;
+            }
+            else
+            {
+                isTurnPage = false;
             }
         }
 
