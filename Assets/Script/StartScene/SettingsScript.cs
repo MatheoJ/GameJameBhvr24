5517e27dd8d9a45a9ef6ee60797011bce956b9b0
HugoFoucrit <hugo.foucrit@gmail.com>
2024-04-14 14:11:54 -0400

    settingsmenu

---
+++ Assets/Script/StartScene/SettingsScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class SettingsScript : MonoBehaviour
+{
+
+    //Start button
+    public Button startButton;
+    public Button creditsButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(OnStartGame);
+        creditsButton.onClick.AddListener(OnCredits);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+    // This method is called when the start button is clicked
+    private void OnStartGame()
+    {
+        SceneManager.LoadScene("PlayerControlTest");
+    }
+    private void OnCredits()
+    {
+        SceneManager.LoadScene("CreditsScene");
+    }
+    private void OnRules()
+    {
+        SceneManager.LoadScene("Rules");
+    }
+}
