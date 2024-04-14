11ab5ef3096e7d8fdc5a75575b0c0af9a3049d6e
MatheoJ <matheo.joseph.01@gmail.com>
2024-04-14 12:46:35 -0400

    add Architecture assets

--- Assets/Script/FoodProjectile.cs
+++ Assets/Script/FoodProjectile.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class FoodProjectile : MonoBehaviour
 {
     public float life = 3;
+    public VisualEffect ImpactVfx;
 
     void Awake()
     {
         Destroy(gameObject, life);
+        if (ImpactVfx != null)
+        {
+            ImpactVfx.Stop();
+        }
     }
 
     void OnCollisionEnter(Collision collision)
@@ -23,6 +29,32 @@ public class FoodProjectile : MonoBehaviour
     }
     void OnTriggerEnter(UnityEngine.Collider other)
     {
+        if (other.gameObject.tag == "Untagged")
+        {
+            //PlayImpactEffect();
+        }
+
+        Debug.Log("Hit: " + other.gameObject.tag);
+
         Destroy(gameObject);
     }
+
+    private void PlayImpactEffect()
+    {
+        if (ImpactVfx != null)
+        {
+            Debug.Log("PlayImpactEffect");
+
+            // Detach the VFX so it does not get destroyed with the GameObject
+            ImpactVfx.transform.parent = null;
+            ImpactVfx.transform.position.Set(transform.position.x, transform.position.y -1, transform.position.z);
+
+            // Play the VFX
+            ImpactVfx.Play();
+
+            // Destroy the VFX after 1 second
+            Destroy(ImpactVfx.gameObject, 1f);
+        }
+    }
+
 }
